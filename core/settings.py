491e43d6915e3b268d0fc2f61f6df28600450245
Inv0kerGod <122999445+Inv0kerGod@users.noreply.github.com>
2024-08-14 20:54:13 +0600

    dz

--- core/settings.py
+++ core/settings.py
@@ -91,14 +91,24 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get("DB_NAME"),
+#         'USER': os.environ.get("DB_USER"),
+#         'PASSWORD': os.environ.get("DB_USER_PASSWORD"),
+#         'HOST': os.environ.get("DB_HOST"),
+#         'PORT': os.environ.get("DB_PORT"),
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get("DB_NAME"),
-        'USER': os.environ.get("DB_USER"),
-        'PASSWORD': os.environ.get("DB_USER_PASSWORD"),
-        'HOST': os.environ.get("DB_HOST"),
-        'PORT': os.environ.get("DB_PORT"),
+        'NAME': "gek_db",
+        'USER': "ainz",
+        'PASSWORD': "54321",
+        'HOST': "localhost",
+        'PORT': "5432",
     }
 }
 
